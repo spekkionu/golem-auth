23a92c0ffab519779607c5ae25eee5515d71d5b1
Jonathan Bernardi <jon@jonbernardi.com>
2016-10-20 04:42:49 -0700

    add tests for storage adapters

---
+++ tests/Storage/MemoryStorageTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Golem\Auth\Test\Storage;
+
+use Golem\Auth\Storage\MemoryStorage;
+
+class MemoryStorageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var MemoryStorage
+     */
+    private $storage;
+
+    public function setUp()
+    {
+        $this->storage = new MemoryStorage();
+    }
+
+    public function test_storing_and_reading()
+    {
+        $this->storage->store(1);
+        $this->assertEquals(1, $this->storage->read());
+    }
+
+    public function test_checking_if_data_exists()
+    {
+        $this->assertFalse($this->storage->exists());
+        $this->storage->store(1);
+        $this->assertTrue($this->storage->exists());
+    }
+
+    public function test_clearing_data()
+    {
+        $this->storage->store(1);
+        $this->assertTrue($this->storage->exists());
+        $this->storage->clear();
+        $this->assertFalse($this->storage->exists());
+    }
+
+    public function test_reading_returns_null_when_there_is_no_data()
+    {
+        $this->assertNull($this->storage->read());
+    }
+}
---
+++ tests/Storage/NativeSessionTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace Golem\Auth\Test\Storage;
+
+use Golem\Auth\Storage\NativeSession;
+
+class NativeSessionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var NativeSession
+     */
+    private $storage;
+
+    public function setUp()
+    {
+        if (isset($_SESSION['session_key'])) {
+            unset($_SESSION['session_key']);
+        }
+        $this->storage = new NativeSession('session_key');
+    }
+
+    public function tearDown()
+    {
+        unset($_SESSION['session_key']);
+    }
+
+    public function test_storing_and_reading()
+    {
+        $this->storage->store(1);
+        $this->assertEquals(1, $this->storage->read());
+    }
+
+    public function test_checking_if_data_exists()
+    {
+        $this->assertFalse($this->storage->exists());
+        $this->storage->store(1);
+        $this->assertTrue($this->storage->exists());
+    }
+
+    public function test_clearing_data()
+    {
+        $this->storage->store(1);
+        $this->assertTrue($this->storage->exists());
+        $this->storage->clear();
+        $this->assertFalse($this->storage->exists());
+    }
+
+    public function test_reading_returns_null_when_there_is_no_data()
+    {
+        $this->assertNull($this->storage->read());
+    }
+}
